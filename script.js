d3ece0cef776a507776bdb57567fdc8a5986acf2
[purrple durrple] <durrple@tuta.io>
2023-05-14 20:23:46 -0500

    added starting code to js file and added psuedo code

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  console.log ("Button has been clicked!");
+
+// 1. Prompt the user for password criteria
+//    a. password length between 8 < 128
+//    b. lowercase, uppercase, numeric, special characters
+// 2. validate the input
+// 3. generate the password
+
+
+// 4. display the generated password to the page
+  return "Generated password will go here!";
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
